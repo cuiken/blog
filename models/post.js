2f7187f319595c3842babc2bc52501a767ebc550
ken.cui <ken.cuik@gmail.com>
2014-01-03 07:08:41 +0800

    加入markdown

--- models/post.js
+++ models/post.js
@@ -1,7 +1,8 @@
 /**
  * Created by ken.cui on 14-1-2.
  */
-var mongodb = require('./db');
+var mongodb = require('./db'),
+    markdown = require('markdown').markdown;
 
 function Post(name, title, post) {
     this.name = name;
@@ -70,6 +71,9 @@ Post.get = function (name, callback) {
                     if (err) {
                         return callback(err);
                     }
+                    docs.forEach(function (doc) {
+                        doc.post = markdown.toHTML(doc.post);
+                    })
                     callback(null, docs);
                 });
         });
