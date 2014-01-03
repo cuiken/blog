6497043bffa8b35392a55b9bdb8c30001faa4b5e
ken.cui <ken.cuik@gmail.com>
2014-01-03 15:47:51 +0800

    留言

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@
 var crypto = require('crypto'),
     User = require('../models/User.js'),
     Post = require('../models/Post.js'),
+    Comment = require('../models/Comment.js'),
     formidable = require('formidable'),
     fs = require('fs');
 
@@ -242,6 +243,28 @@ module.exports = function (app) {
             }
             req.flash('success', '删除成功!');
             res.redirect('/');
+        });
+    });
+
+    app.post('/u/:name/:day/:title', function (req, res) {
+        var date = new Date(),
+            time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+                date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+        var comment = {
+            name: req.body.name,
+            email: req.body.email,
+            website: req.body.website,
+            time: time,
+            content: req.body.content
+        };
+        var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+        newComment.save(function (err) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            req.flash('success', '留言成功!');
+            res.redirect('back');
         });
     });
 
