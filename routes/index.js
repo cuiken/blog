4be96d1a2c4ebc8056e8598e37888e874a12013c
ken.cui <ken.cuik@gmail.com>
2014-01-03 16:24:13 +0800

    分页功能

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ var crypto = require('crypto'),
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        Post.getAll(null, function (err, posts) {
+        var page = req.query.p ? Number(req.query.p) : 1;
+        Post.getTen(null, page, function (err, posts, total) {
             if (err) {
                 posts = [];
             }
@@ -19,6 +20,9 @@ module.exports = function (app) {
                 title: '主页',
                 user: req.session.user,
                 posts: posts,
+                page: page,
+                isFirstPage: (page - 1) === 0,
+                isLastPage: ((page - 1) * 10 + posts.length) === total,
                 success: req.flash('success').toString(),
                 error: req.flash('error').toString()
             });
@@ -164,12 +168,13 @@ module.exports = function (app) {
     });
 
     app.get('/u/:name', function (req, res) {
+        var page = req.query.p ? Number(req.query.p) : 1;
         User.get(req.params.name, function (err, user) {
             if (!user) {
                 req.flash('error', '用户不存在!');
                 return res.redirect('/');
             }
-            Post.getAll(user.name, function (err, posts) {
+            Post.getTen(user.name, page, function (err, posts, total) {
                 if (err) {
                     req.flash('error', err);
                     return res.redirect('/');
@@ -177,6 +182,9 @@ module.exports = function (app) {
                 res.render('user', {
                     title: user.name,
                     posts: posts,
+                    page: page,
+                    isFirstPage: (page - 1) === 0,
+                    isLastPage: ((page - 1) * 10 + posts.length) === total,
                     user: req.session.user,
                     success: req.flash('success').toString(),
                     error: req.flash('error').toString()
